092db0b3cc1ecc472f2d78e83a93d2670b718932
RaphCodes1 <raphaelcreer@gmail.com>
2025-01-08 23:48:57 +0400

    added stuff to do

--- philo/main.c
+++ philo/main.c
@@ -1,6 +1,21 @@
 #include "Philosophers.h"
 
 int main(int ac, char **av)
-{
-
+{   
+    if(ac == 5 || ac == 6)
+    {
+        printf("welcome to philo\n");
+        
+        //check av if valid arguments
+        // convert av to numbers
+        /* make a struct with these values
+        Number of philos
+        time a philo will die if he does not eat
+        time for philo to eat
+        time for philo to sleep
+        number of times philo need to eat vefore ending the program
+        */
+    }
+    else
+        printf("enter valid arguments :)\n");
 }
