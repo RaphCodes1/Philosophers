25f20138f2c693369d9e9d9a8ef4f4b42791f1b6
RaphCodes1 <raphaelcreer@gmail.com>
2025-01-16 20:54:35 +0400

    initialization of the struct

--- philo/Philosophers.h
+++ philo/Philosophers.h
@@ -6,12 +6,33 @@
 typedef struct s_philo
 {
     pthread_t	thread;
+	int 		id;
+	int			eating;
+	int			meals_eaten;
+	int			last_meal;
 	int			num_of_philos;
 	int			time_to_die;
 	int			time_to_eat;
 	int			time_to_sleep;
 	int			num_times_to_eat;
+	int			start_time;
+	int			*dead;
+	pthread_mutex_t *r_fork;
+	pthread_mutex_t *l_fork;
+	pthread_mutex_t *write_lock;
+	pthread_mutex_t *dead_lock;
+	pthread_mutex_t *meal_lock;
 }   t_philo;
+
+typedef struct s_program
+{
+	int				dead_flag;
+	pthread_mutex_t dead_lock;
+	pthread_mutex_t meal_lock;
+	pthread_mutex_t write_lock;
+	t_philo			*philos;
+}	t_program;
+
 // helpers
 int	ft_atoi(const char *nptr);
 size_t	ft_strlen(const char *s);
--- philo/main.c
+++ philo/main.c
@@ -23,51 +23,97 @@ int check_valid_args(char **av)
 	}
 	return (0);
 }
-void philo_struct_int(t_philo *philo, char **av)
+
+void int_argv_content(t_philo *philo, char **av)
 {	
+	philo->num_of_philos = ft_atoi(av[1]);
+	philo->time_to_die = ft_atoi(av[2]);
+	philo->time_to_eat = ft_atoi(av[3]);
+	philo->time_to_sleep = ft_atoi(av[4]);
+	if(av[5])
+		philo->num_times_to_eat = ft_atoi(av[5]);
+	else
+		philo->num_times_to_eat = -1;
+
+}
+void philo_struct_int(t_philo **philo, t_program *program, pthread_mutex_t *forks, char **av)
+{	
+	int num_of_philo;
 	int i;
 	int holder;
 
-	i = 1;
-	philo = malloc(sizeof(t_philo));
-	if(!philo)
+	i = 0;
+	num_of_philo = ft_atoi(av[1]);
+	(*philo) = malloc(num_of_philo * sizeof(t_philo));
+	if(!(*philo))
 		perror("Malloc Fail");
-	while(av[i])
-	{	
-		holder = ft_atoi(av[i]);
-		printf("holder: %d\n",holder);
-		if(i == 1)
-			philo->num_of_philos = holder;
-		else if(i == 2)
-			philo->time_to_die = holder;
-		else if(i == 3)
-			philo->time_to_eat = holder;
-		else if(i == 4)
-			philo->time_to_sleep = holder;
-		else if(i == 5)
-			philo->num_times_to_eat = holder;
+	while(i < num_of_philo)
+	{
+		(*philo)[i].id = i + 1;
+		(*philo)[i].eating = 0;
+		(*philo)[i].meals_eaten = 0;
+		int_argv_content(&(*philo)[i],av);
+		(*philo)[i].start_time = 0; // get_curr_time();
+		(*philo)[i].last_meal = 0; // get_curr_time();
+		(*philo)[i].write_lock = &program->write_lock;
+		(*philo)[i].dead_lock = &program->dead_lock;
+		(*philo)[i].meal_lock = &program->meal_lock;
+		if(i == 0)
+			(*philo)[i].r_fork = &forks[(*philo)->num_of_philos - 1];
+		else
+			(*philo)[i].r_fork = &forks[i - 1];
 		i++;
 	}
 }
-//check av if valid arguments
-// convert av to numbers
-/* make a struct with these values
-Number of philos
-time a philo will die if he does not eat
-time for philo to eat
-time for philo to sleep
-number of times philo need to eat vefore ending the program
-*/
+
+void program_init(t_program *program, t_philo *philos)
+{
+	program->dead_flag = 0;
+	program->philos = philos;
+	pthread_mutex_init(&program->dead_lock, NULL);
+	pthread_mutex_init(&program->meal_lock, NULL);
+	pthread_mutex_init(&program->write_lock, NULL);
+}
+
+void forks_init(pthread_mutex_t **forks, int num_of_philos)
+{
+	int i;
+
+	i = 0;
+	(*forks) = malloc(num_of_philos * sizeof(pthread_mutex_t));
+	if(!(*forks))
+		perror("malloc failed!");
+	while(i < num_of_philos)
+	{
+		pthread_mutex_init(&(*forks)[i], NULL);
+		i++;
+	}
+}
+
 int main(int ac, char **av)
 {   
-	t_philo *philo;
-
+	t_philo 	*philos;
+	t_program	program;
+	pthread_mutex_t	*forks;
     if(ac == 5 || ac == 6)
-    {
-        printf("welcome to philo\n");
+    {	
+		
 		if(!check_valid_args(av))
+		{	
+			program_init(&program,philos);
+			forks_init(&forks, ft_atoi(av[1]));
+			philo_struct_int(&philos, &program, forks, av);
+		}
+		int i = 0;
+		
+		while(i < 5)
 		{
-			philo_struct_int(philo, av);
+			printf("id: %d\n", philos[i].id);
+			printf("num of philos: %d\n", philos[i].num_of_philos);
+			printf("time to die: %d\n", philos[i].time_to_die);
+			printf("time to sleep: %d\n", philos[i].time_to_sleep);
+			printf("time to eat: %d\n", philos[i].num_times_to_eat);
+			i++;
 		}
     }
     else
